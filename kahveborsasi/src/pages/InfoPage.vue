8fa952bd27ce17ce400af93dd4cc0586a9150cc8
nedimgorgu <118492020+kadirruygun@users.noreply.github.com>
2023-12-13 14:47:30 +0300

    Footer eklendi

---
+++ kahveborsasi/src/pages/InfoPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Navbar/>
+        <NavbarBottom  class="q-mb-lg"/>
+        <InfoComponent/>
+    </div>
+</template>
+
+<script>
+
+import Navbar from "../components/NavbarComponent.vue";
+import NavbarBottom from "../components/NavbarBottom.vue";
+import InfoComponent from "../components/InfoComponent.vue"
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+
+components:{  Navbar,NavbarBottom,InfoComponent }
+})
+</script>
